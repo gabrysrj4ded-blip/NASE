dc4831dd2fa1325f3148a844f4b3e32765cfd251
gabrysrj4ded-blip <gabrysrj4ded@gmail.com>
2026-07-15 17:03:02 +0330

    Fix icon animation

--- components/home/HomeScreen.tsx
+++ components/home/HomeScreen.tsx
@@ -10,18 +10,10 @@ export default function HomeScreen() {
     <main className="mx-auto flex min-h-screen w-full max-w-[430px] flex-col px-5 pt-[max(12px,env(safe-area-inset-top))]">
 
       <Header />
-
-      <div className="-mt-3">
        
       <SearchBar />
-      
-      <div className="-mt-2">
 
       <SectionDivider />
-     
-      <div className="-mt-2">
-        
-      </div>
       
       <HeroBanner />
 
