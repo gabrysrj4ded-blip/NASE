285c1411e6a5b446c6326167725db4d75db3e951
gabrysrj4ded-blip <gabrysrj4ded@gmail.com>
2026-07-15 13:54:20 +0330

    Fix icon animation

--- components/ui/SectionDivider.tsx
+++ components/ui/SectionDivider.tsx
@@ -1,25 +1,29 @@
 export default function SectionDivider() {
   return (
-    <div className="relative my-5 flex items-center justify-center">
+    <div className="my-5 flex items-center justify-center">
+      <div className="relative h-px w-full overflow-hidden">
 
-      {/* Left line */}
+        <div
+          className="
+            absolute inset-0
+            bg-gradient-to-r
+            from-transparent
+            via-white/16
+            to-transparent
+          "
+        />
 
-      <div className="h-px flex-1 bg-gradient-to-r from-transparent via-white/10 to-white/4" />
-
-      {/* Center glow */}
-
-      <div className="relative mx-4 flex h-3 w-3 items-center justify-center">
-
-        <div className="absolute h-5 w-5 rounded-full bg-white/[0.05] blur-md" />
-
-        <div className="absolute h-2 w-2 rounded-full bg-white/70 shadow-[0_0_12px_rgba(255,255,255,.65)]" />
+        <div
+          className="
+            absolute left-1/2 top-1/2
+            h-px w-40
+            -translate-x-1/2 -translate-y-1/2
+            bg-white/8
+            blur-[2px]
+          "
+        />
 
       </div>
-
-      {/* Right line */}
-
-      <div className="h-px flex-1 bg-gradient-to-l from-transparent via-white/10 to-white/4" />
-
     </div>
   );
 }
